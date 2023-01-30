cca7c5151aad8158dbf9fe503f3267d8101b7b6a
Joshua Trusty <nanoman657@gmail.com>
2023-01-30 12:39:32 -0600

    Refactor function names

--- submission.py
+++ submission.py
@@ -1,4 +1,6 @@
-def entries_in_proximity(row: int, col: int, proximity: int, matrix: list[list]) -> set:
+def get_entries_in_proximity(
+    row: int, col: int, proximity: int, matrix: list[list]
+) -> set:
     nearby_entries = set()
     row_range = range(max(0, row - proximity), min(row + proximity + 1, len(matrix)))
     col_range = range(max(0, col - proximity), min(col + proximity + 1, len(matrix[0])))
@@ -10,7 +12,7 @@ def entries_in_proximity(row: int, col: int, proximity: int, matrix: list[list])
     return nearby_entries
 
 
-def positive_entries_in_proximity(
+def get_positive_entries_in_proximity(
     matrix: list[list[int]], distance_threshhold: int
 ) -> int:
     m = len(matrix)
@@ -20,7 +22,7 @@ def positive_entries_in_proximity(
         for j in range(n):
             if matrix[i][j] > 0:
                 nearby_entries = nearby_entries.union(
-                    entries_in_proximity(i, j, distance_threshhold, matrix)
+                    get_entries_in_proximity(i, j, distance_threshhold, matrix)
                 )
     return len(nearby_entries)
 
@@ -54,30 +56,30 @@ matrix_with_inf = [
 matrix_with_min_size = [[0, 0], [0, 0]]
 
 
-error_message = "The function did not return the correct number of cells."
+# Test main solution function
 
 assert (
-    positive_entries_in_proximity(example_matrix, 1) == 7
+    get_positive_entries_in_proximity(example_matrix, 1) == 7
 ), "Function failed with the example matrix."
 assert (
-    positive_entries_in_proximity(matrix_with_ones, 1) == 15
+    get_positive_entries_in_proximity(matrix_with_ones, 1) == 15
 ), "Function failed with matrix with all positive values."
 assert (
-    positive_entries_in_proximity(matrix_with_ones, 0) == 15
+    get_positive_entries_in_proximity(matrix_with_ones, 0) == 15
 ), """Function failed with matrix with all positive values
     and zero proximity requirement"""
 assert (
-    positive_entries_in_proximity(matrix_with_zeroes, 1) == 0
+    get_positive_entries_in_proximity(matrix_with_zeroes, 1) == 0
 ), "Function failed with matrix with all zero values."
 assert (
-    positive_entries_in_proximity(matrix_with_neg_zeroes, 1) == 0
+    get_positive_entries_in_proximity(matrix_with_neg_zeroes, 1) == 0
 ), "Function failed with matrix with all zero and negative values."
 assert (
-    positive_entries_in_proximity(matrix_with_neg_zeroes_pos, 1) == 4
+    get_positive_entries_in_proximity(matrix_with_neg_zeroes_pos, 1) == 4
 ), "Function failed with matrix with negative, zero, and positive values."
 assert (
-    positive_entries_in_proximity(matrix_with_inf, 1) == 4
+    get_positive_entries_in_proximity(matrix_with_inf, 1) == 4
 ), "Function failed with matrix containing an infinite positive value."
 assert (
-    positive_entries_in_proximity(matrix_with_min_size, 0) == 0
+    get_positive_entries_in_proximity(matrix_with_min_size, 0) == 0
 ), "Function failed with matrix of minimum size."
